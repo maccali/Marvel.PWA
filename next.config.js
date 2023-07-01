eccaf5eed06ba9fbf459e892b876ca6f7f5308de
maccali <guilherme.maccali@universo.univates.br>
2023-07-01 16:38:41 -0300

    fix: ajust new pwa config for next 13

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require("next-pwa");
+const path = require("path");
+const withPWAInit = require("next-pwa");
 const runtimeCaching = require("next-pwa/cache");
 
 const cacheTime = 365 * 24 * 60 * 60; // 365 days
@@ -14,11 +15,43 @@ runtimeCaching.map((item) => {
   }
 });
 
+const generateAppDirEntry = (entry) => {
+  const packagePath = require.resolve("next-pwa");
+  const packageDirectory = path.dirname(packagePath);
+  const registerJs = path.join(packageDirectory, "register.js");
+
+  return entry().then((entries) => {
+    // Register SW on App directory, solution: https://github.com/shadowwalker/next-pwa/pull/427
+    if (entries["main-app"] && !entries["main-app"].includes(registerJs)) {
+      if (Array.isArray(entries["main-app"])) {
+        entries["main-app"].unshift(registerJs);
+      } else if (typeof entries["main-app"] === "string") {
+        entries["main-app"] = [registerJs, entries["main-app"]];
+      }
+    }
+    return entries;
+  });
+};
+
+const withPWA = withPWAInit({
+  dest: "public",
+  runtimeCaching,
+  // Solution: https://github.com/shadowwalker/next-pwa/issues/424#issuecomment-1399683017
+  buildExcludes: ["app-build-manifest.json"],
+  mode: "production",
+});
+
 module.exports = withPWA({
-  pwa: {
-    dest: "public",
-    runtimeCaching,
-    mode: "production",
+  // pwa: {
+  //   dest: "public",
+  //   runtimeCaching,
+  //   mode: "production",
+  // },
+  webpack: (config) => {
+    const entry = generateAppDirEntry(config.entry);
+    config.entry = () => entry;
+
+    return config;
   },
   env: {
     MARVEL_PUBLIC_KEY: process.env.MARVEL_PUBLIC_KEY,
